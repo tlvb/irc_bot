3100db431b0d90aa9ed127f1ea2497d51896f6b9
Leo Bärring <leo.barring@protonmail.com>
2016-12-04 03:36:50 +0100

    adding ability to timeout before e.g. 001 welcome completion
    Signed-off-by: Leo Bärring <leo.barring@protonmail.com>

--- IRCBot/Plugin/PingPong.pm
+++ IRCBot/Plugin/PingPong.pm
@@ -9,9 +9,9 @@ sub new { #{{{
 	my $class = shift;
 
 	my $self = IRCBot::Plugin::PluginBase->new(@_);
-	@{$self}{qw/version last_time key/} = ('0.1.0', time, '-');
+	@{$self}{qw/version last_time key/} = ('0.2.0', time, '-');
 	bless $self, $class;
-	$self->state_change('dormant');
+	$self->state_change('init');
 
 	return $self;
 
@@ -21,7 +21,7 @@ sub handle_message { #{{{
 	my $m = shift;
 
 	if ($m->c eq '001' or $m->c eq 'PING') {
-		$self->state_change('idle') if $self->{state} eq 'dormant';
+		$self->state_change('idle') if $self->{state} eq 'init';
 	}
 
 	if ($m->c eq 'PING') {
@@ -41,7 +41,11 @@ sub handle_message { #{{{
 sub handle_timeout { #{{{
 	my $self = shift;
 	my $now = time;
-	if ($self->{state} eq 'idle') {
+	if ($self->{state} eq 'init') {
+		$self->log_d('timeout during init phase');
+		$self->emit_event(type=>'GLOBAL-RESET');
+	}
+	elsif ($self->{state} eq 'idle') {
 		if ($now - $self->{last_time} >= $self->config->{query_threshold}) {
 			$self->state_change('ping-sent');
 			$self->{key} = 1000000 + int(rand(1000000));
@@ -68,7 +72,10 @@ sub handle_timeout { #{{{
 sub get_timeout { #{{{
 	my $self = shift;
 	my $timeout = -1;
-	if ($self->{state} eq 'idle') {
+	if ($self->{state} eq 'init') {
+		$timeout =  $self->config->{query_threshold}*3 - (time - $self->{last_time});
+	}
+	elsif ($self->{state} eq 'idle') {
 		$timeout =  $self->config->{query_threshold} - (time - $self->{last_time});
 	}
 	elsif ($self->{state} eq 'ping-sent' || $self->{state} eq 'ping-quit') {
